d35ba73cbf6143ecef6695d4f82d6a23d666b857
Sérgio <santossergio759@gmail.com>
2020-08-22 15:31:47 -0300

    publish website

--- main.js
+++ main.js
@@ -1,13 +1,5 @@
 const express = require('express');
 const app = express();
-
-app.use((req, res, next) => { //Cria um middleware onde todas as requests passam por ele 
-    if ((req.headers["x-forwarded-proto"] || "").endsWith("http")) //Checa se o protocolo informado nos headers é HTTP 
-        res.redirect(`https://${req.headers.host}${req.url}`); //Redireciona pra HTTPS 
-    else //Se a requisição já é HTTPS 
-        next(); //Não precisa redirecionar, passa para os próximos middlewares que servirão com o conteúdo desejado 
-});
-
 const favicon = require('serve-favicon');
 
 app.use(express.static("public"));
